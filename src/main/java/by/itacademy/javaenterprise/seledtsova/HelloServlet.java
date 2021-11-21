d49cf4a71a5728829b396d4e428c69b7d39062cd
olgaseled9 <olgaseledtsova9@yandex.by>
2021-11-21 18:07:24 +0300

    add readme and gitignore

--- src/main/java/by/itacademy/javaenterprise/seledtsova/HelloServlet.java
+++ src/main/java/by/itacademy/javaenterprise/seledtsova/HelloServlet.java
@@ -25,6 +25,5 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 }
